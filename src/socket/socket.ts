ff6e12616ca0df12fd3836f3e6c45478fee99f11
nithinraj1999 <vnithinraj99@gmail.com>
2024-12-20 12:37:59 +0530

    latest

--- src/socket/socket.ts
+++ src/socket/socket.ts
@@ -6,6 +6,7 @@ const SOCKET_SERVER_URL = import.meta.env.VITE_SOCKET_SERVER_URL;
 console.log("socket....",SOCKET_SERVER_URL);
 
 const socket = io(SOCKET_SERVER_URL, {
+  transports : ['websocket'],
   withCredentials: true, 
 });
 
