f79cea2ab848f156d93c645a5eb71e2faa7b2fcb
nithinraj1999 <vnithinraj99@gmail.com>
2025-04-26 09:18:12 +0530

    refactor: removed unnecesary comments

--- src/components/client/JobPost/JobPostForm.tsx
+++ src/components/client/JobPost/JobPostForm.tsx
@@ -16,21 +16,19 @@ import "react-quill/dist/quill.core.css";
 
 const JobPostForm: React.FC = () => {
   const [title, setTitle] = useState("");
-  const [category, setCategory] = useState("");
-  const [subCategory, setSubCategory] = useState("");
   const [description, setDescription] = useState("");
   const [selectedFile, setSelectedFile] = useState<File | null>(null);
   const [isDragging, setIsDragging] = useState(false);
-  const fileInputRef = useRef<HTMLInputElement>(null); // Reference to the hidden input
+  const fileInputRef = useRef<HTMLInputElement>(null);
   const [skillInput, setSkillInput] = useState<string>("");
-  const [skillList, setSkillList] = useState<string[]>([]); // State for storing the list of skills
+  const [skillList, setSkillList] = useState<string[]>([]); 
   const [selectedLevel, setSelectedLevel] = useState<string>("");
   const [selectedPaymentType, setSelectedPaymentType] = useState<string>("");
-  const [hourlyRateFrom, setHourlyRateFrom] = useState<number | "">(""); // State for "From"
-  const [hourlyRateTo, setHourlyRateTo] = useState<number | "">(""); // State for "To"
-  const [totalAmount, setTotalAmount] = useState<number | "">(""); // State for fixed total amount
+  const [hourlyRateFrom, setHourlyRateFrom] = useState<number | "">(""); 
+  const [hourlyRateTo, setHourlyRateTo] = useState<number | "">("");
+  const [totalAmount, setTotalAmount] = useState<number | "">(""); 
   const [errors, setErrors] = useState<Record<string, string>>({});
-  const { user } = useSelector((state: RootState) => state.user); // Get user from Redux store
+  const { user } = useSelector((state: RootState) => state.user); 
   const navigate = useNavigate();
   const dispatch = useDispatch();
 
@@ -60,38 +58,36 @@ const JobPostForm: React.FC = () => {
 
       hourlyRateFrom: z
         .number()
-        .positive("Hourly rate must be a positive number") // Use positive() for clarity
+        .positive("Hourly rate must be a positive number") 
         .optional(),
 
       hourlyRateTo: z
         .number()
-        .positive("Hourly rate must be a positive number") // Use positive() for clarity
+        .positive("Hourly rate must be a positive number") 
         .optional(),
 
       totalAmount: z
         .number()
-        .positive("Total amount must be a positive number") // Use positive() for clarity
+        .positive("Total amount must be a positive number")
         .optional(),
     })
     .refine(
       (data) => {
         if (data.paymentType === "hourly") {
-          // Ensure both hourlyRateFrom and hourlyRateTo are provided for hourly payment type
           return (
             data.hourlyRateFrom !== undefined &&
             data.hourlyRateTo !== undefined &&
             data.hourlyRateFrom > 0 &&
             data.hourlyRateTo > 0
-          ); // Ensure positive numbers
+          ); 
         } else if (data.paymentType === "fixed") {
-          // Ensure totalAmount is provided for fixed payment type
-          return data.totalAmount !== undefined && data.totalAmount > 0; // Ensure positive number
+          return data.totalAmount !== undefined && data.totalAmount > 0; 
         }
-        return true; // If paymentType is not set, return true (not expected)
+        return true; 
       },
       {
         message: "",
-        path: ["paymentType"], // Specify where to show the error
+        path: ["paymentType"], 
       }
     );
 
@@ -195,8 +191,8 @@ const JobPostForm: React.FC = () => {
       postData.append("paymentType", selectedPaymentType);
 
       if (selectedPaymentType === "hourly") {
-        postData.append("hourlyPrice[from]", hourlyRateFrom.toString()); // Use the correct nested structure
-        postData.append("hourlyPrice[to]", hourlyRateTo.toString()); // Use the correct nested structure
+        postData.append("hourlyPrice[from]", hourlyRateFrom.toString()); 
+        postData.append("hourlyPrice[to]", hourlyRateTo.toString()); 
       } else if (selectedPaymentType === "fixed") {
         postData.append("fixedPrice", totalAmount.toString());
       }
@@ -216,7 +212,7 @@ const JobPostForm: React.FC = () => {
     }
   };
 
-  const [predefinedSkills, setPredefinedSkills] = useState<string[]>([]); // Initialize as an empty array of strings
+  const [predefinedSkills, setPredefinedSkills] = useState<string[]>([]); 
 
   useEffect(() => {
     async function getSkills() {
@@ -259,252 +255,6 @@ const JobPostForm: React.FC = () => {
     setDescription(description);
   };
   return (
-    // <>
-    //   <div className="min-h-screen flex items-center justify-center">
-    //     <div className="bg-slate-100 h-full w-3/4 py-3">
-    //       <form className="px-16" onSubmit={handleSubmit}>
-    //         <div>
-    //           <h3 className="text-3xl font-bold">freelix</h3>
-    //           <h1 className="text-6xl whitespace-nowrap font-extrabold mt-10">
-    //             Tell us what you
-    //             <br />
-    //             need done.
-    //           </h1>
-    //           <br />
-    //           <p className="text-slate-400">
-    //             we'll guide you to create the brief. The more detail the better
-    //           </p>
-    //           <br />
-    //         </div>
-    //         <div>
-    //           <h2 className="text-lg font-bold">
-    //             Write a title for your job post
-    //           </h2>
-    //           <input
-    //             placeholder="ex. Build a responsive WordPress website "
-    //             className="w-full h-10 mt-1 px-2"
-    //             value={title}
-    //             onChange={(e) => setTitle(e.target.value)}
-    //           ></input>
-    //           {errors.title && <p className="text-red-500">{errors.title}</p>}
-    //           <h3 className="text-slate-500 mt-1">Example titles</h3>
-    //           <ul className="list-disc pl-8 text-slate-400 mt-1">
-    //             <li>
-    //               Build responsive WordPress site with booking/payment
-    //               functionality
-    //             </li>
-    //             <li>AR experience needed for virtual product demos (ARCore)</li>
-    //             <li>
-    //               Developer needed to update Android app UI for new OS/device
-    //               specs
-    //             </li>
-    //           </ul>
-    //         </div>
-
-    //         <div>
-    //           <h3 className="text-lg font-bold mt-6">Description</h3>
-
-    //           <ReactQuill
-    //             theme="snow"
-    //             value={description}
-    //             onChange={saveDescription}
-
-    //             placeholder="Write something..."
-    //           />
-    //           {errors.description && (
-    //             <p className="text-red-500">{errors.description}</p>
-    //           )}
-    //         </div>
-
-    //         <div>
-    //           <h3 className="text-lg font-bold mt-14">
-    //             What are the main skills required for your work?
-    //           </h3>
-
-    //           <div>
-    //             <input
-    //               placeholder="Search skills"
-    //               value={searchInput}
-    //               onChange={handleInputChange}
-    //               className="h-10 mt-1 px-2 w-1/2"
-    //             />
-    //             {suggestedSkills.length > 0 && (
-    //               <ul className="absolute bg-white border border-gray-300 rounded-md mt-1 w-1/2 z-10">
-    //                 {suggestedSkills.map((skill, index) => (
-    //                   <li
-    //                     key={index}
-    //                     onClick={() => selectSkill(skill)}
-    //                     className="cursor-pointer hover:bg-blue-100 px-2 py-1"
-    //                   >
-    //                     {skill}
-    //                   </li>
-    //                 ))}
-    //               </ul>
-    //             )}
-    //           </div>
-
-    //           <div>
-    //             <h3 className="text-slate-500 mt-6">Selected Skills</h3>
-    //             <div className="flex flex-wrap mt-2">
-    //               {skillList.map((skill) => (
-    //                 <div
-    //                   key={skill}
-    //                   className="flex items-center bg-white rounded-full px-3 py-1 mr-2 mb-2"
-    //                 >
-    //                   <p className="text-sm text-blue-800">{skill}</p>
-    //                   <button
-    //                     onClick={(event) => handleRemoveSkill(skill, event)}
-    //                     className="ml-2 text-blue-600 hover:text-blue-800"
-    //                   >
-    //                     &times; {/* Close icon */}
-    //                   </button>
-    //                 </div>
-    //               ))}
-    //             </div>
-    //           </div>
-
-    //           <div>
-    //             <h3 className="text-lg font-bold mt-6">
-    //               Level of Experience you want
-    //             </h3>
-    //             <div className="flex space-x-4 h-40 mt-6">
-    //               {["Beginner", "Intermediate", "Experienced"].map((level) => (
-    //                 <label
-    //                   key={level}
-    //                   className={`flex-1 p-3 rounded-lg transition-colors duration-300 cursor-pointer ${
-    //                     selectedLevel === level
-    //                       ? "  border-2 border-blue-800" // Style for selected
-    //                       : "bg-gray-200 text-gray-800 hover:bg-gray-300"
-    //                   }`}
-    //                 >
-    //                   <input
-    //                     type="radio"
-    //                     name="skill-level"
-    //                     value={level}
-    //                     checked={selectedLevel === level}
-    //                     onChange={() => handleChange(level)}
-    //                     className="hidden" // Hide the default radio button
-    //                   />
-    //                   {level}
-    //                 </label>
-    //               ))}
-    //             </div>
-    //             {errors.experience && (
-    //               <p className="text-red-500">{errors.experience}</p>
-    //             )}
-    //           </div>
-    //           <div className="mb-32">
-    //             <h3 className="text-lg font-bold mt-6">
-    //               Tell us about your budget.
-    //             </h3>
-    //             <p className="text-slate-500 mt-1">
-    //               This will help us match you to talent within your range.
-    //             </p>
-    //             <div>
-    //               <div className="flex space-x-4 mb-4">
-    //                 <label className="flex-1 cursor-pointer">
-    //                   <input
-    //                     type="radio"
-    //                     name="payment-type"
-    //                     value="hourly"
-    //                     checked={selectedPaymentType === "hourly"}
-    //                     onChange={() => onPaymentTypeChange("hourly")}
-    //                     className="hidden" // Hide the default radio button
-    //                   />
-    //                   <div
-    //                     className={`p-3 rounded-lg transition-colors duration-300 ${
-    //                       selectedPaymentType === "hourly"
-    //                         ? "bg-blue-600 text-white border-2 border-blue-800"
-    //                         : "bg-gray-200 text-gray-800 hover:bg-gray-300"
-    //                     }`}
-    //                   >
-    //                     Hourly Pay
-    //                   </div>
-    //                 </label>
-
-    //                 <label className="flex-1 cursor-pointer">
-    //                   <input
-    //                     type="radio"
-    //                     name="payment-type"
-    //                     value="fixed"
-    //                     checked={selectedPaymentType === "fixed"}
-    //                     onChange={() => onPaymentTypeChange("fixed")}
-    //                     className="hidden" // Hide the default radio button
-    //                   />
-    //                   <div
-    //                     className={`p-3 rounded-lg transition-colors duration-300 ${
-    //                       selectedPaymentType === "fixed"
-    //                         ? "bg-blue-600 text-white border-2 border-blue-800"
-    //                         : "bg-gray-200 text-gray-800 hover:bg-gray-300"
-    //                     }`}
-    //                   >
-    //                     Fixed Price
-    //                   </div>
-    //                 </label>
-    //               </div>
-    //               {errors.paymentType && (
-    //                 <p className="text-red-500">{errors.paymentType}</p>
-    //               )}
-
-    //               {selectedPaymentType === "hourly" && (
-    //                 <div className="flex space-x-4">
-    //                   <input
-    //                     type="number"
-    //                     placeholder="From"
-    //                     className="p-3 border rounded-lg w-1/2"
-    //                     value={hourlyRateFrom} // Use state value
-    //                     onChange={(e) =>
-    //                       setHourlyRateFrom(
-    //                         e.target.value ? parseFloat(e.target.value) : ""
-    //                       )
-    //                     } // Parse to float
-    //                   />
-    //                   <input
-    //                     type="number"
-    //                     placeholder="To"
-    //                     className="p-3 border rounded-lg w-1/2"
-    //                     value={hourlyRateTo} // Use state value
-    //                     onChange={(e) =>
-    //                       setHourlyRateTo(
-    //                         e.target.value ? parseFloat(e.target.value) : ""
-    //                       )
-    //                     } // Parse to float
-    //                   />
-    //                   {errors.hourlyRateFrom && (
-    //                     <p className="text-red-500">{errors.hourlyRateFrom}</p>
-    //                   )}
-    //                 </div>
-    //               )}
-
-    //               {selectedPaymentType === "fixed" && (
-    //                 <div className="flex space-x-4 mt-4">
-    //                   <input
-    //                     type="number"
-    //                     placeholder="Total Amount"
-    //                     className="p-3 border rounded-lg w-full"
-    //                     value={totalAmount} // Use state value
-    //                     onChange={(e) =>
-    //                       setTotalAmount(
-    //                         e.target.value ? parseFloat(e.target.value) : ""
-    //                       )
-    //                     } // Update state
-    //                   />
-    //                 </div>
-    //               )}
-    //               {errors.totalAmount && (
-    //                 <p className="text-red-500">{errors.totalAmount}</p>
-    //               )}
-    //             </div>
-    //           </div>
-    //         </div>
-    //         <button type="submit" className="bg-black text-white  h-10 w-20">
-    //           Submit{" "}
-    //         </button>
-    //       </form>
-    //     </div>
-    //   </div>
-    //   <ToastContainer />
-    // </>
     <>
       <div className="min-h-screen flex items-center justify-center bg-gray-50">
         <div className="bg-white shadow-lg rounded-lg w-11/12 max-w-4xl py-6">
